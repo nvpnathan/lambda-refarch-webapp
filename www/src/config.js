088175356255e3e6cd117459c2d2492ee18616a9
Nathan Ness <nathan79@gmail.com>
2021-02-11 08:47:54 -0700

    remove quote

--- www/src/config.js
+++ www/src/config.js
@@ -2,7 +2,7 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "2qiujgqeegacas5plddvajkfof"",     // CognitoClientID
+  "aws_user_pools_web_client_id": "2qiujgqeegacas5plddvajkfof",     // CognitoClientID
   "api_base_url": "https://1myo3musk9.execute-api.us-west-1.amazonaws.com/Prod",              // TodoFunctionApi
   "cognito_hosted_domain": "mytodoappdemo-nessnab2.auth.us-west-1.amazoncognito.com",        // CognitoDomainName
   "redirect_url": "https://master.d102imxlea4chn.amplifyapp.com"                  // AmplifyURL
