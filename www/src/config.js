f5ccb37ed1ff891b8dba2fd7c3d74cf138007021
Nathan Ness <nathan79@gmail.com>
2021-02-11 09:56:44 -0700

    changed stage

--- www/src/config.js
+++ www/src/config.js
@@ -3,7 +3,7 @@
 
 const config = {
   "aws_user_pools_web_client_id": "70p8ai0meivlj9jjrnhor15v0j",     // CognitoClientID
-  "api_base_url": "https://td9gl0hst0.execute-api.us-west-1.amazonaws.com/Prod",              // TodoFunctionApi
+  "api_base_url": "https://td9gl0hst0.execute-api.us-west-1.amazonaws.com/prod",              // TodoFunctionApi
   "cognito_hosted_domain": "https://mytodoappdemo-nessnab2.auth.us-west-1.amazoncognito.com",        // CognitoDomainName
   "redirect_url": "https://master.d14of5ysmob0y6.amplifyapp.com"                  // AmplifyURL
 };
