228df72deeff3e4150e0aa89362b60302eb3703a
Ness <nessn@147dda19a012.ant.amazon.com>
2021-02-11 08:32:26 -0700

    updated config values

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "None",     // CognitoClientID
-  "api_base_url": "None",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "None",                   // CognitoDomainName
-  "redirect_url": "None"                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "2qiujgqeegacas5plddvajkfof"",     // CognitoClientID
+  "api_base_url": "https://1myo3musk9.execute-api.us-west-1.amazonaws.com/Prod",              // TodoFunctionApi
+  "cognito_hosted_domain": "mytodoappdemo-nessnab2.auth.us-west-1.amazoncognito.com",        // CognitoDomainName
+  "redirect_url": "https://master.d102imxlea4chn.amplifyapp.com"                  // AmplifyURL
 };
 
 export default config;
