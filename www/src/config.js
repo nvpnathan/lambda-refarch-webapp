63218024dd3b75efb25af8931c68477d766f9b2a
Nathan Ness <nathan79@gmail.com>
2021-02-27 13:10:01 -0700

    updated urls

--- www/src/config.js
+++ www/src/config.js
@@ -4,7 +4,7 @@
 
 const config = {
   "aws_user_pools_web_client_id": "2nlh8pmi148536u6n9obbua5li",     // CognitoClientID
-  "api_base_url": "https://drith2dvz9.execute-api.us-west-1.amazonaws.com/prod",              // TodoFunctionApi
+  "api_base_url": "https://5r65yq8vh0.execute-api.us-west-1.amazonaws.com/prod",              // TodoFunctionApi
   "cognito_hosted_domain": "mytodoappdemo-ab2test.auth.us-west-1.amazoncognito.com",        // CognitoDomainName
   "redirect_url": "https://dev.d36egbw5m4iyv3.amplifyapp.com"                  // AmplifyURL
 };
