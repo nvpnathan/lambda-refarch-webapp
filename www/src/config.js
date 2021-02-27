8560bf3f031286db03c81ba2ac5172dd8468b0f6
Nathan Ness <nathan79@gmail.com>
2021-02-26 17:30:34 -0700

    new build

--- www/src/config.js
+++ www/src/config.js
@@ -4,9 +4,9 @@
 
 const config = {
   "aws_user_pools_web_client_id": "1320dlfovar5mrnk9j1bf7l508",     // CognitoClientID
-  "api_base_url": "https://jh6hg7ua64.execute-api.us-west-1.amazonaws.com/prod",              // TodoFunctionApi
-  "cognito_hosted_domain": "	mytodoappdemo-taskapp.auth.us-west-1.amazoncognito.com",        // CognitoDomainName
-  "redirect_url": "https://dev.d23pkbb6au9zqt.amplifyapp.com"                  // AmplifyURL
+  "api_base_url": "https://bw3r71yzrj.execute-api.us-west-1.amazonaws.com/prod",              // TodoFunctionApi
+  "cognito_hosted_domain": "mytodoappdemo-taskapp.auth.us-west-1.amazoncognito.com",        // CognitoDomainName
+  "redirect_url": "https://dev.d2r6i6v3nebkbh.amplifyapp.com"                  // AmplifyURL
 };
 
 export default config;
