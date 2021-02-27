5f68ca208303f38377e2f8b65b7d3fc75e1182f7
Nathan Ness <nathan79@gmail.com>
2021-02-27 13:24:48 -0700

    updated urls

--- www/src/config.js
+++ www/src/config.js
@@ -3,7 +3,7 @@
 
 const config = {
   "aws_user_pools_web_client_id": "1t9s5d9al223nppgh9vvt35fkr",     // CognitoClientID
-  "api_base_url": "https://5r65yq8vh0.execute-api.us-west-1.amazonaws.com/prod",              // TodoFunctionApi
+  "api_base_url": "https://drith2dvz9.execute-api.us-west-1.amazonaws.com/prod",              // TodoFunctionApi
   "cognito_hosted_domain": "mytaskappdemo-taskapp.auth.us-west-1.amazoncognito.com",        // CognitoDomainName
   "redirect_url": "https://master.d1vrk7hr431g6t.amplifyapp.com"                  // AmplifyURL
 };
