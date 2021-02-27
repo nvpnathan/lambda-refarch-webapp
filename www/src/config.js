9806c662e80f01ebf0d9e5f213c87c6b03064f33
Nathan Ness <nathan79@gmail.com>
2021-02-27 15:28:05 -0700

    task deploy

--- www/src/config.js
+++ www/src/config.js
@@ -5,7 +5,7 @@ const config = {
   "aws_user_pools_web_client_id": "6gq449d4urn3i67m6tfl9gc2on",     // CognitoClientID
   "api_base_url": "ttps://wetqx5rgvb.execute-api.us-west-1.amazonaws.com/prod",              // TodoFunctionApi
   "cognito_hosted_domain": "mytaskappdemo-taskapp.auth.us-west-1.amazoncognito.com",        // CognitoDomainName
-  "redirect_url": "hhttps://master.d378wctf090ono.amplifyapp.com"                  // AmplifyURL
+  "redirect_url": "https://master.d378wctf090ono.amplifyapp.com"                  // AmplifyURL
 };
 
 export default config;
