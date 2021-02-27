b012017ca4d44037a5d8ecd51200c5bd73644536
Nathan Ness <nathan79@gmail.com>
2021-02-27 15:56:22 -0700

    task deploy

--- www/src/config.js
+++ www/src/config.js
@@ -6,7 +6,7 @@ const config = {
   "aws_user_pools_web_client_id": "292qsmjt01cs5s3arkaqisroda",     // CognitoClientID
   "api_base_url": "https://f0isz52bah.execute-api.us-west-1.amazonaws.com/prod",              // TodoFunctionApi
   "cognito_hosted_domain": "mytodoappdemo-ab2test.auth.us-west-1.amazoncognito.com",        // CognitoDomainName
-  "redirect_url": "https://dev.d33355i6j3qlr5.amplifyapp.com"                  // AmplifyURL
+  "redirect_url": "https://master.d378wctf090ono.amplifyapp.com/"                  // AmplifyURL
 };
 
 export default config;
