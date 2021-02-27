4dc16a27ab84a597142e7871b100ebb512701d08
Nathan Ness <nathan79@gmail.com>
2021-02-27 16:27:12 -0700

    changed api url

--- www/src/config.js
+++ www/src/config.js
@@ -3,9 +3,9 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "292qsmjt01cs5s3arkaqisroda",     // CognitoClientID
+  "aws_user_pools_web_client_id": "6gq449d4urn3i67m6tfl9gc2on",     // CognitoClientID
   "api_base_url": "https://wetqx5rgvb.execute-api.us-west-1.amazonaws.com/prod",              // TodoFunctionApi
-  "cognito_hosted_domain": "mytodoappdemo-ab2test.auth.us-west-1.amazoncognito.com",        // CognitoDomainName
+  "cognito_hosted_domain": "mytaskappdemo-taskapp.auth.us-west-1.amazoncognito.com",        // CognitoDomainName
   "redirect_url": "https://dev.d33355i6j3qlr5.amplifyapp.com"                  // AmplifyURL
 };
 
