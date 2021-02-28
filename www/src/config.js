638d3b3c4450cc824ca30068a6a0e4fb8caaea4d
Nathan Ness <nathan79@gmail.com>
2021-02-27 18:19:22 -0700

    updated urls

--- www/src/config.js
+++ www/src/config.js
@@ -3,10 +3,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "6gq449d4urn3i67m6tfl9gc2on",     // CognitoClientID
-  "api_base_url": "https://wetqx5rgvb.execute-api.us-west-1.amazonaws.com/prod",              // TodoFunctionApi
+  "aws_user_pools_web_client_id": "3ihp7fjkld0plvoau7bc0be8hs",     // CognitoClientID
+  "api_base_url": "https://lqu32u41k9.execute-api.us-west-1.amazonaws.com/prod",              // TodoFunctionApi
   "cognito_hosted_domain": "mytaskappdemo-taskapp.auth.us-west-1.amazoncognito.com",        // CognitoDomainName
-  "redirect_url": "https://master.d378wctf090ono.amplifyapp.com/"                  // AmplifyURL
+  "redirect_url": "https://master.d3mcvrwn51r4a3.amplifyapp.com"                  // AmplifyURL
 };
 
 export default config;
